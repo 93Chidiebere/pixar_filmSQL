09e5ec3530a0baa79943c9e107026095e54360a3
93chidiebere <vincent.christopher.189736@unn.edu.ng>
2026-01-14 16:57:46 +0100

    Updated query with more questions and answers

--- Pixar_films2.sql
+++ Pixar_films2.sql
@@ -6,6 +6,7 @@ Expected skill level: Data Analyst / Business Analyst
 */
 SELECT * FROM films
 SELECT * FROM box_office
+SELECT * FROM academy
 --- Questions:
 --- 1. Calculate the average box office revenue per film.
 SELECT AVG(CAST(box_office_worldwide AS decimal(18,2))) AS Average_Revenue
@@ -62,3 +63,58 @@ ON f.film = b.film) AS lowest_revenue_film
 
 ORDER BY box_office_worldwide ASC;
 
+
+--- Join pixar_films with pixar_box_office to show film title and worldwide gross.
+SELECT f.film, b.box_office_worldwide
+
+FROM films f
+
+JOIN box_office b
+
+ON f.film = b.film;
+
+
+--- Show the average runtime per rating category.
+SELECT film_rating, AVG(CAST(run_time AS decimal(5,2))) AS avg_runtime
+
+FROM films
+
+WHERE run_time > 0  --- To exclude bad data
+
+GROUP BY film_rating
+
+ORDER BY avg_runtime DESC;
+
+
+--- Retrieve films that earned more than the average box office revenue.
+SELECT film, CAST(box_office_worldwide AS DECIMAL(18,2)) AS revenue
+
+FROM box_office
+
+WHERE box_office_worldwide > (SELECT AVG(CAST(box_office_worldwide AS DECIMAL(18,2))) AS avg_revenue
+
+FROM box_office)
+
+ORDER BY revenue DESC;
+
+
+--- Find films that won at least one award.
+SELECT DISTINCT film
+
+FROM academy
+
+WHERE [status] = 'Won';
+
+--- ADV Version.
+SELECT film, COUNT(*) AS awards_won
+
+FROM academy
+
+WHERE [status] = 'Won'
+
+GROUP BY film
+
+HAVING COUNT(*) >= 1;
+
+
+--- 
